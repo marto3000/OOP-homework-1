0ec3e68b077f626afa724b40a7a8a1662a0e325d
Martin Nikolov <martooo3000@gmail.com>
2023-04-20 20:24:20 +0300

    -added class "Plant" with some values and functions

--- functions.cpp
+++ functions.cpp
@@ -1,3 +1,40 @@
 #include <iostream>
 #include "header.h"
 
+//PLANT
+
+Plant::Plant(char* newName, char newSunPrefference[MAX_SUN_PREF_LENG], int newWatering)
+{
+	int nameLeng = strlen(newName) + 1;
+	name = new char[nameLeng];
+	strcpy_s(name, nameLeng, newName);
+	strcpy_s(sunPrefference, MAX_SUN_PREF_LENG, newSunPrefference);
+	watering = newWatering;
+}
+
+Plant::Plant(Plant const& obj)
+{
+	int nameLeng = strlen(obj.name) + 1;
+	name = new char[nameLeng];
+	strcpy_s(name, nameLeng, obj.name);
+	strcpy_s(sunPrefference, MAX_SUN_PREF_LENG, obj.sunPrefference);
+	watering = obj.watering;
+}
+
+Plant& Plant::operator= (Plant const& obj)
+{
+	int nameLeng = strlen(obj.name) + 1;
+	delete[] name;
+	name = new char[nameLeng];
+	strcpy_s(name, nameLeng, obj.name);
+	strcpy_s(sunPrefference, MAX_SUN_PREF_LENG, obj.sunPrefference);
+	watering = obj.watering;
+
+	return *this;
+}
+
+Plant::~Plant()
+{
+	delete[] name;
+	name = nullptr;
+}
--- header.h
+++ header.h
@@ -1,6 +1,19 @@
 #ifndef HEADER
 #define HEADER
 
+const int MAX_SUN_PREF_LENG = 8;
 
+class Plant
+{
+	char* name;
+	char sunPrefference[MAX_SUN_PREF_LENG];
+	int watering;
+	//int count;
+public:
+	Plant(char* newName, char newSunPrefference[MAX_SUN_PREF_LENG], int newWatering);
+	Plant(Plant const& obj);
+	Plant& operator= (Plant const& obj);
+	~Plant();
+};
 
 #endif // !HEADER
